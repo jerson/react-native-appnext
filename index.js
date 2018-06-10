27913fbfd0c9a67b189d149d8eeb382a79a3415c
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2018-06-09 21:28:30 -0500

    added extra logs

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { RNAppNext } = NativeModules;
 
 const TAG = "[Appnext]";
 export default class Appnext {
-  static emitter = RNAppNext ?  new NativeEventEmitter(RNAppNext) : null;
+  static emitter = RNAppNext ? new NativeEventEmitter(RNAppNext) : null;
 
   static eventHandlers = {
     onAdLoaded: new Map(),
@@ -20,98 +20,121 @@ export default class Appnext {
 
   static setupAd(placementId) {
     __DEV__ && console.debug(TAG, "setupAd", placementId);
-    if(Platform.OS!=='android'){
-      return
+    if (Platform.OS !== "android") {
+      __DEV__ && console.warn(TAG, "not supported on", Platform.OS);
+
+      return;
     }
     return RNAppNext.setupAd(placementId);
   }
 
   static showRewardedVideo(placementId) {
     __DEV__ && console.debug(TAG, "showRewardedVideo", placementId);
-    if(Platform.OS!=='android'){
-      return
+    if (Platform.OS !== "android") {
+      __DEV__ && console.warn(TAG, "not supported on", Platform.OS);
+
+      return;
     }
     return RNAppNext.showRewardedVideo(placementId);
   }
 
   static showInterstitial(placementId) {
     __DEV__ && console.debug(TAG, "showInterstitial", placementId);
-    if(Platform.OS!=='android'){
-      return
+    if (Platform.OS !== "android") {
+      __DEV__ && console.warn(TAG, "not supported on", Platform.OS);
+
+      return;
     }
     return RNAppNext.showInterstitial(placementId);
   }
 
   static showFullScreenVideo(placementId) {
     __DEV__ && console.debug(TAG, "showFullScreenVideo", placementId);
-    if(Platform.OS!=='android'){
-      return
+    if (Platform.OS !== "android") {
+      __DEV__ && console.warn(TAG, "not supported on", Platform.OS);
+
+      return;
     }
     return RNAppNext.showFullScreenVideo(placementId);
   }
 
   static loadAd(category = "Entertainment") {
     __DEV__ && console.debug(TAG, "loadAd", category);
-    if(Platform.OS!=='android'){
-      return
+    if (Platform.OS !== "android") {
+      __DEV__ && console.warn(TAG, "not supported on", Platform.OS);
+
+      return;
     }
     return RNAppNext.loadAd(category);
   }
 
   static removeAd(adId) {
     __DEV__ && console.debug(TAG, "removeAd", adId);
-    if(Platform.OS!=='android'){
-      return
+    if (Platform.OS !== "android") {
+      __DEV__ && console.warn(TAG, "not supported on", Platform.OS);
+
+      return;
     }
-    return  RNAppNext.removeAd(adId);
+    return RNAppNext.removeAd(adId);
   }
 
   static adClicked(adId) {
     __DEV__ && console.debug(TAG, "adClicked", adId);
-    if(Platform.OS!=='android'){
-      return
+    if (Platform.OS !== "android") {
+      __DEV__ && console.warn(TAG, "not supported on", Platform.OS);
+
+      return;
     }
     return RNAppNext.adClicked(adId);
   }
 
   static adImpression(adId) {
     __DEV__ && console.debug(TAG, "adImpression", adId);
-    if(Platform.OS!=='android'){
-      return
+    if (Platform.OS !== "android") {
+      __DEV__ && console.warn(TAG, "not supported on", Platform.OS);
+
+      return;
     }
     return RNAppNext.adImpression(adId);
   }
 
   static videoStarted(adId) {
     __DEV__ && console.debug(TAG, "videoStarted", adId);
-    if(Platform.OS!=='android'){
-      return
+    if (Platform.OS !== "android") {
+      __DEV__ && console.warn(TAG, "not supported on", Platform.OS);
+
+      return;
     }
     return RNAppNext.videoStarted(adId);
   }
 
   static videoEnded(adId) {
     __DEV__ && console.debug(TAG, "videoEnded", adId);
-    if(Platform.OS!=='android'){
-      return
+    if (Platform.OS !== "android") {
+      __DEV__ && console.warn(TAG, "not supported on", Platform.OS);
+
+      return;
     }
     return RNAppNext.videoEnded(adId);
   }
 
   static privacyClicked(adId) {
     __DEV__ && console.debug(TAG, "privacyClicked", adId);
-    if(Platform.OS!=='android'){
-      return
+    if (Platform.OS !== "android") {
+      __DEV__ && console.warn(TAG, "not supported on", Platform.OS);
+
+      return;
     }
     return RNAppNext.privacyClicked(adId);
   }
 
   static addEventListener(type, handler) {
     if (this.eventHandlers[type]) {
-      this.emitter && this.eventHandlers[type].set(
-        handler,
-        this.emitter.addListener(type, handler)
-      );
+      this.emitter &&
+        this.eventHandlers[type].set(
+          handler,
+          this.emitter.addListener(type, handler)
+        );
     } else {
       console.warn(`Event with type ${type} does not exist.`);
     }
@@ -123,7 +146,7 @@ export default class Appnext {
     }
     this.eventHandlers[type].get(handler).remove();
     this.eventHandlers[type].delete(handler);
-  } 
+  }
 
   static removeAllListeners() {
     let names = Object.keys(eventHandlers);
